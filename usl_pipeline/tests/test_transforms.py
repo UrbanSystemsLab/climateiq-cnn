e20499f39fcfc605be9c18f9723c1bf899f45cf3
Walt Askew <waltaskew@google.com>
2024-03-27 00:37:00 +0000

    start building out the gcp transforms

---
+++ usl_pipeline/tests/test_transforms.py
@@ -0,0 +1,69 @@
+import io
+import tarfile
+from unittest import mock
+
+import functions_framework
+import numpy
+import rasterio
+
+from usl_pipeline import transforms
+
+
+@mock.patch.object(transforms.storage, "Client", autospec=True)
+def test_build_feature_matrix(mock_client):
+    # Get some random data to place in a tiff file.
+    height = 5
+    width = 3
+    tiff_array = numpy.random.randint(
+        low=0, high=16, size=(height, width), dtype=numpy.uint8
+    )
+
+    # Build an in-memory tiff file and grab its bytes.
+    with rasterio.io.MemoryFile() as memfile:
+        with memfile.open(
+            driver="GTiff",
+            width=width,
+            height=height,
+            count=1,
+            dtype=rasterio.uint8,
+        ) as raster:
+            raster.write(tiff_array.astype(rasterio.uint8), 1)
+        tiff_bytes = memfile.read()
+
+    # Place the tiff file bytes into an archive.
+    archive = io.BytesIO()
+    with tarfile.open(fileobj=archive, mode="w") as tar:
+        tf = tarfile.TarInfo("elevation.tiff")
+        tf.size = len(tiff_bytes)
+        tar.addfile(tf, io.BytesIO(tiff_bytes))
+    # Seek to the beginning so the file can be read.
+    archive.seek(0)
+
+    # Set the archive to be returned by the mock GCS client.
+    mock_client().bucket("").blob("").open.return_value = archive
+    mock_client.reset_mock()
+
+    transforms.build_feature_matrix(
+        functions_framework.CloudEvent(
+            {"source": "test", "type": "event"},
+            data={"bucket": "bucket", "name": "map/chunks/name.tar"},
+        )
+    )
+
+    # Ensure we worked with the right GCP paths.
+    mock_client.assert_has_calls(
+        [
+            mock.call().bucket("bucket"),
+            mock.call().bucket().blob("map/chunks/name.tar"),
+            mock.call().bucket().blob().open("rb"),
+            mock.call().bucket().blob("map/features/name.npy"),
+            mock.call().bucket().blob().upload_from_file(mock.ANY),
+        ]
+    )
+
+    # Ensure we attempted to upload a serialized matrix of the tiff.
+    mock_client().bucket("").blob("").upload_from_file.assert_called_once()
+    uploaded_array = numpy.load(
+        mock_client().bucket("").blob("").upload_from_file.call_args[0][0]
+    )
+    numpy.testing.assert_array_equal(uploaded_array, tiff_array)
