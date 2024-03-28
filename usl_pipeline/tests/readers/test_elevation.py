9dc02f1b4f5c86766ed8f1b1f6087583fed8e738
Roman Sutormin <rsutormin@google.com>
2024-03-28 23:32:57 +0000

    Elevation reader from Esri ASCII

--- usl_pipeline/tests/readers/test_elevation.py
+++ usl_pipeline/tests/readers/test_elevation.py
@@ -1,10 +1,12 @@
+import io
+
 import numpy
 from numpy.testing import assert_array_equal
 import rasterio
 from rasterio import Affine, CRS
 from rasterio.io import MemoryFile
 
-from usl_pipeline.readers.elevation import read_from_geotiff
+from usl_pipeline.readers.elevation import read_from_geotiff, read_from_esri_ascii
 from usl_pipeline.shared.geo_data import ElevationHeader
 
 
@@ -63,6 +65,61 @@ def test_load_elevation_from_geotiff_with_changed_no_data():
             cell_size=2.0,
             nodata_value=-9999.0,
             crs=CRS({"init": "EPSG:32618"}),
+        )
+        assert_array_equal(
+            elevation.data,
+            [
+                [-9999.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+            ],
+        )
+
+
+def prepare_test_esri_ascii_file_wrapper():
+    lines = [
+        "ncols 3",
+        "nrows 2",
+        "xllcorner 100.0",
+        "yllcorner 496.0",
+        "cellsize 2.0",
+        "NODATA_value 0.0",
+        "0.0 1.0 2.0",
+        "3.0 4.0 5.0",
+    ]
+    reader = io.BufferedReader(io.BytesIO("\n".join(lines).encode("utf-8")))
+    return io.TextIOWrapper(reader)
+
+
+def test_load_elevation_from_esri_ascii_default_no_data():
+    with prepare_test_esri_ascii_file_wrapper() as file:
+        elevation = read_from_esri_ascii(file)
+        assert elevation.header == ElevationHeader(
+            col_count=3,
+            row_count=2,
+            x_ll_corner=100.0,
+            y_ll_corner=496.0,
+            cell_size=2.0,
+            nodata_value=0.0,
+        )
+        assert_array_equal(
+            elevation.data,
+            [
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+            ],
+        )
+
+
+def test_load_elevation_from_esri_ascii_with_changed_no_data():
+    with prepare_test_esri_ascii_file_wrapper() as file:
+        elevation = read_from_esri_ascii(file, no_data_value=-9999.0)
+        assert elevation.header == ElevationHeader(
+            col_count=3,
+            row_count=2,
+            x_ll_corner=100.0,
+            y_ll_corner=496.0,
+            cell_size=2.0,
+            nodata_value=-9999.0,
         )
         assert_array_equal(
             elevation.data,
--- usl_pipeline/usl_pipeline/readers/elevation.py
+++ usl_pipeline/usl_pipeline/readers/elevation.py
@@ -1,6 +1,7 @@
 import typing
 from typing import Optional
 
+import numpy
 import rasterio.features
 
 from usl_pipeline.shared.geo_data import Elevation
@@ -13,7 +14,7 @@ def read_from_geotiff(
     band: int = 1,
     no_data_value: Optional[float] = None,
 ) -> Elevation:
-    """Loading elevation raster data from GeoTIFF file.
+    """Loading elevation raster data from GeoTIFF file/stream.
 
     Args:
         file: Binary stream to load from.
@@ -33,24 +34,66 @@ def read_from_geotiff(
         transform = src.transform
         ll_corner = transform * (0, src.height)
         input_nodata = src.nodata
-        final_nodata_value = (
-            no_data_value if no_data_value is not None else input_nodata
-        )
         elv_header = ElevationHeader(
             col_count=src.width,
             row_count=src.height,
             x_ll_corner=ll_corner[0],
             y_ll_corner=ll_corner[1],
             cell_size=transform[0],
-            nodata_value=final_nodata_value,
+            nodata_value=(no_data_value if no_data_value is not None else input_nodata),
             crs=src.crs,
         )
         elv_data = None
 
         if not header_only:
-            data = src.read(band)
+            elv_data = src.read(band)
             if no_data_value is not None:
-                data[data == input_nodata] = no_data_value
-            elv_data = data
+                elv_data[elv_data == input_nodata] = no_data_value
 
         return Elevation(header=elv_header, data=elv_data)
+
+
+def read_from_esri_ascii(
+    file: typing.TextIO,
+    header_only: bool = False,
+    no_data_value: Optional[float] = None,
+) -> Elevation:
+    """Loading elevation raster data from Esri ASCII file/stream.
+
+    Args:
+        file: Binary stream to load from.
+        header_only: Indicates that only header should be loaded, whereas
+            elevation data should be skipped.
+        no_data_value: Optional value to set in the returned data to indicate
+            absence of data. If not supplied, the no-data value defined in the
+            TIFF file itself will be used.
+
+    Returns:
+        Elevation object.
+    """
+    print(type(file))
+    # read 6 header lines
+    header_map = {}
+    for row_index in range(0, 6):
+        line = file.readline()
+        # split into key/value pair
+        parts = line.split(" ", 1)
+        header_map[parts[0].lower()] = parts[1]
+    input_nodata = float(header_map["nodata_value"])
+    header = ElevationHeader(
+        col_count=int(header_map["ncols"]),
+        row_count=int(header_map["nrows"]),
+        x_ll_corner=float(header_map["xllcorner"]),
+        y_ll_corner=float(header_map["yllcorner"]),
+        cell_size=float(header_map["cellsize"]),
+        nodata_value=(no_data_value if no_data_value is not None else input_nodata),
+    )
+
+    # read data matrix
+    data = None
+    if not header_only:
+        data = numpy.loadtxt(file, dtype="float32")
+        if no_data_value is not None:
+            data[data == input_nodata] = no_data_value
+
+    return Elevation(header=header, data=data)
