be88492a9b2e47e466d7aeaee8bc7482381c0292
Roman Sutormin <rsutormin@google.com>
2024-04-01 20:33:25 +0000

    More fixes in GeoTiff to elevation readers

--- usl_pipeline/tests/readers/test_elevation_readers.py
+++ usl_pipeline/tests/readers/test_elevation_readers.py
@@ -1,16 +1,15 @@
 import io
 
 import numpy
-from numpy.testing import assert_array_equal
+from numpy import testing
 import rasterio
-from rasterio import Affine, CRS
-from rasterio.io import MemoryFile
+import rasterio.io
 
-from usl_pipeline.readers.elevation import read_from_geotiff, read_from_esri_ascii
-from usl_pipeline.shared.geo_data import ElevationHeader
+from usl_pipeline.readers import elevation_readers
+from usl_pipeline.shared import geo_data
 
 
-def prepare_test_geotiff_elevation_memory_file(mem_file: MemoryFile):
+def prepare_test_geotiff_elevation_memory_file(mem_file: rasterio.io.MemoryFile):
     tiff_array = numpy.array(
         [
             [0.0, 1.0, 2.0],
@@ -24,8 +23,8 @@ def prepare_test_geotiff_elevation_memory_file(mem_file: MemoryFile):
         width=3,
         height=2,
         count=1,
-        crs=CRS.from_epsg(32618),
-        transform=Affine(2.0, 0.0, 100.0, 0.0, -2.0, 500.0),
+        crs=rasterio.CRS.from_epsg(32618),
+        transform=rasterio.Affine(2.0, 0.0, 100.0, 0.0, -2.0, 500.0),
     ) as raster:
         raster.write(tiff_array.astype(rasterio.float32), 1)
 
@@ -33,17 +32,17 @@ def prepare_test_geotiff_elevation_memory_file(mem_file: MemoryFile):
 def test_load_elevation_from_geotiff_default_no_data():
     with rasterio.io.MemoryFile() as mem_file:
         prepare_test_geotiff_elevation_memory_file(mem_file)
-        elevation = read_from_geotiff(mem_file)
-        assert elevation.header == ElevationHeader(
+        elevation = elevation_readers.read_from_geotiff(mem_file)
+        assert elevation.header == geo_data.ElevationHeader(
             col_count=3,
             row_count=2,
             x_ll_corner=100.0,
             y_ll_corner=496.0,
             cell_size=2.0,
             nodata_value=0.0,
-            crs=CRS({"init": "EPSG:32618"}),
+            crs=rasterio.CRS({"init": "EPSG:32618"}),
         )
-        assert_array_equal(
+        testing.assert_array_equal(
             elevation.data,
             [
                 [0.0, 1.0, 2.0],
@@ -56,17 +55,17 @@ def test_load_elevation_from_geotiff_default_no_data():
 def test_load_elevation_from_geotiff_with_changed_no_data():
     with rasterio.io.MemoryFile() as mem_file:
         prepare_test_geotiff_elevation_memory_file(mem_file)
-        elevation = read_from_geotiff(mem_file, no_data_value=-9999.0)
-        assert elevation.header == ElevationHeader(
+        elevation = elevation_readers.read_from_geotiff(mem_file, no_data_value=-9999.0)
+        assert elevation.header == geo_data.ElevationHeader(
             col_count=3,
             row_count=2,
             x_ll_corner=100.0,
             y_ll_corner=496.0,
             cell_size=2.0,
             nodata_value=-9999.0,
-            crs=CRS({"init": "EPSG:32618"}),
+            crs=rasterio.CRS({"init": "EPSG:32618"}),
         )
-        assert_array_equal(
+        testing.assert_array_equal(
             elevation.data,
             [
                 [-9999.0, 1.0, 2.0],
@@ -92,8 +91,8 @@ def prepare_test_esri_ascii_file_wrapper():
 
 def test_load_elevation_from_esri_ascii_default_no_data():
     with prepare_test_esri_ascii_file_wrapper() as file:
-        elevation = read_from_esri_ascii(file)
-        assert elevation.header == ElevationHeader(
+        elevation = elevation_readers.read_from_esri_ascii(file)
+        assert elevation.header == geo_data.ElevationHeader(
             col_count=3,
             row_count=2,
             x_ll_corner=100.0,
@@ -101,7 +100,7 @@ def test_load_elevation_from_esri_ascii_default_no_data():
             cell_size=2.0,
             nodata_value=0.0,
         )
-        assert_array_equal(
+        testing.assert_array_equal(
             elevation.data,
             [
                 [0.0, 1.0, 2.0],
@@ -112,8 +111,8 @@ def test_load_elevation_from_esri_ascii_default_no_data():
 
 def test_load_elevation_from_esri_ascii_with_changed_no_data():
     with prepare_test_esri_ascii_file_wrapper() as file:
-        elevation = read_from_esri_ascii(file, no_data_value=-9999.0)
-        assert elevation.header == ElevationHeader(
+        elevation = elevation_readers.read_from_esri_ascii(file, no_data_value=-9999.0)
+        assert elevation.header == geo_data.ElevationHeader(
             col_count=3,
             row_count=2,
             x_ll_corner=100.0,
@@ -121,7 +120,7 @@ def test_load_elevation_from_esri_ascii_with_changed_no_data():
             cell_size=2.0,
             nodata_value=-9999.0,
         )
-        assert_array_equal(
+        testing.assert_array_equal(
             elevation.data,
             [
                 [-9999.0, 1.0, 2.0],
--- usl_pipeline/usl_pipeline/readers/elevation_readers.py
+++ usl_pipeline/usl_pipeline/readers/elevation_readers.py
@@ -1,29 +1,27 @@
 import typing
-from typing import Optional
 
 import numpy
 import rasterio.features
 
-from usl_pipeline.shared.geo_data import Elevation
-from usl_pipeline.shared.geo_data import ElevationHeader
+from usl_pipeline.shared import geo_data
 
 
 def read_from_geotiff(
     file: typing.BinaryIO,
     header_only: bool = False,
     band: int = 1,
-    no_data_value: Optional[float] = None,
-) -> Elevation:
+    no_data_value: typing.Optional[float] = None,
+) -> geo_data.Elevation:
     """Loading elevation raster data from GeoTIFF file/stream.
 
     Args:
         file: Binary stream to load from.
-        header_only: Indicates that only header should be loaded, whereas
-            elevation data should be skipped.
+        header_only: Indicates that only header should be loaded, whereas elevation data
+            should be skipped.
         band: Index of a band that should be loaded from GeoTIFF.
-        no_data_value: Optional value to set in the returned data to indicate
-            absence of data. If not supplied, the no-data value defined in the
-            TIFF file itself will be used.
+        no_data_value: Optional value to set in the returned data to indicate absence of
+            data. If not supplied, the no-data value defined in the TIFF file itself
+            will be used.
 
     Returns:
         Elevation object.
@@ -33,7 +31,7 @@ def read_from_geotiff(
         transform = src.transform
         ll_corner = transform * (0, src.height)
         input_nodata = src.nodata
-        elv_header = ElevationHeader(
+        elv_header = geo_data.ElevationHeader(
             col_count=src.width,
             row_count=src.height,
             x_ll_corner=ll_corner[0],
@@ -49,23 +47,23 @@ def read_from_geotiff(
             if no_data_value is not None:
                 elv_data[elv_data == input_nodata] = no_data_value
 
-        return Elevation(header=elv_header, data=elv_data)
+        return geo_data.Elevation(header=elv_header, data=elv_data)
 
 
 def read_from_esri_ascii(
     file: typing.TextIO,
     header_only: bool = False,
-    no_data_value: Optional[float] = None,
-) -> Elevation:
+    no_data_value: typing.Optional[float] = None,
+) -> geo_data.Elevation:
     """Loading elevation raster data from Esri ASCII file/stream.
 
     Args:
         file: Binary stream to load from.
-        header_only: Indicates that only header should be loaded, whereas
-            elevation data should be skipped.
-        no_data_value: Optional value to set in the returned data to indicate
-            absence of data. If not supplied, the no-data value defined in the
-            TIFF file itself will be used.
+        header_only: Indicates that only header should be loaded, whereas elevation data
+            should be skipped.
+        no_data_value: Optional value to set in the returned data to indicate absence of
+            data. If not supplied, the no-data value defined in the TIFF file itself
+            will be used.
 
     Returns:
         Elevation object.
@@ -75,10 +73,10 @@ def read_from_esri_ascii(
     for row_index in range(6):
         line = file.readline()
         # split into key/value pair
-        parts = line.split(maxsplit=1)
-        header_map[parts[0].lower()] = parts[1]
+        key, value = line.split(maxsplit=1)
+        header_map[key.lower()] = value
     input_nodata = float(header_map["nodata_value"])
-    header = ElevationHeader(
+    header = geo_data.ElevationHeader(
         col_count=int(header_map["ncols"]),
         row_count=int(header_map["nrows"]),
         x_ll_corner=float(header_map["xllcorner"]),
@@ -94,4 +92,4 @@ def read_from_esri_ascii(
         if no_data_value is not None:
             data[data == input_nodata] = no_data_value
 
-    return Elevation(header=header, data=data)
+    return geo_data.Elevation(header=header, data=data)
