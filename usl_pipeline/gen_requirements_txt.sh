7ac70fedfac4c40d42861f857db7921116881305
Walt Askew <waltaskew@google.com>
2024-04-15 12:38:21 -0700

    Remove usl_pipeline/requirements.txt
    - This isn't a real requirements.txt tied to a real binary, so let's
      remove it rather than confuse folks.

--- usl_pipeline/gen_requirements_txt.sh
+++
@@ -1,5 +0,0 @@
-cat "$(dirname "$0")/map_uploader/requirements.txt" "$(dirname "$0")/cloud_functions/requirements.txt" \
-    | sed "s/..\/usl_lib/usl_lib/" \
-    | LC_COLLATE=C sort \
-    | uniq \
-	  > "$(dirname "$0")/requirements.txt"
