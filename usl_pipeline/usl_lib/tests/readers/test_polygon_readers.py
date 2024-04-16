4930662ca6de8621b716838fc060dc9c42bef666
Roman Sutormin <rsutormin@google.com>
2024-04-16 17:24:37 +0000

    Refactoring of the polygon reader from text file

--- usl_pipeline/usl_lib/tests/readers/test_polygon_readers.py
+++ usl_pipeline/usl_lib/tests/readers/test_polygon_readers.py
@@ -8,13 +8,13 @@ from usl_lib.readers import polygon_readers
 def test_read_polygons_from_text_file_no_masks():
     lines = [
         "2",
-        "4 0.1 0.2 0.2 0.1 1.1 1.1 1.2 1.1  # unused part",
+        "4 0.1 0.2 0.2 0.1 1.1 1.1 1.2 1.1",
         "5 1.0 2.0 2.0 1.0 1.0 10.0 10.0 20.0 20.0 10.0",
         "# Unused line",
     ]
     reader = io.BufferedReader(io.BytesIO("\n".join(lines).encode("utf-8")))
     with io.TextIOWrapper(reader) as file:
-        polygon_masks = polygon_readers.read_polygons_from_text_file(file)
+        polygon_masks = list(polygon_readers.read_polygons_from_text_file(file))
         assert polygon_masks == [
             (geometry.Polygon([(0.1, 1.1), (0.2, 1.1), (0.2, 1.2), (0.1, 1.1)]), 1),
             (geometry.Polygon([(1, 10), (2, 10), (2, 20), (1, 20), (1, 10)]), 1),
@@ -24,15 +24,13 @@ def test_read_polygons_from_text_file_no_masks():
 def test_read_polygons_from_text_file_with_masks():
     lines = [
         "2",
-        "8 4 0.1 0.2 0.2 0.1 1.1 1.1 1.2 1.1  # unused part",
+        "8 4 0.1 0.2 0.2 0.1 1.1 1.1 1.2 1.1",
         "9 5 1.0 2.0 2.0 1.0 1.0 10.0 10.0 20.0 20.0 10.0",
         "# Unused line",
     ]
     reader = io.BufferedReader(io.BytesIO("\n".join(lines).encode("utf-8")))
     with io.TextIOWrapper(reader) as file:
-        polygon_masks = polygon_readers.read_polygons_from_text_file(
-            file, support_mask_values=True
-        )
+        polygon_masks = list(polygon_readers.read_polygons_from_text_file(file))
         assert polygon_masks == [
             (geometry.Polygon([(0.1, 1.1), (0.2, 1.1), (0.2, 1.2), (0.1, 1.1)]), 8),
             (geometry.Polygon([(1, 10), (2, 10), (2, 20), (1, 20), (1, 10)]), 9),
--- usl_pipeline/usl_lib/usl_lib/readers/polygon_readers.py
+++ usl_pipeline/usl_lib/usl_lib/readers/polygon_readers.py
@@ -1,39 +1,39 @@
 import typing
-from typing import Tuple
+from typing import Generator, Tuple
 
 from shapely import geometry
 
 
 def read_polygons_from_text_file(
-    file: typing.TextIO, support_mask_values: bool = False
-) -> list[Tuple[geometry.Polygon, int]]:
+    file: typing.TextIO,
+) -> Generator[Tuple[geometry.Polygon, int], None, None]:
     """Reads polygon information with optional mask values from text file.
+
+    The algorithm detects if the first column in the file contains mask values that are
+    assigned to each polygon. If not, default mask value 1 is used.
 
     Args:
         file: File text stream to load from.
-        support_mask_values: Optional indicator of the presence of polygon mask values
-            in the first column in polygon lines. Default case is that mask column is
-            not present (in this case mask value 1 will be assigned to every polygon).
 
     Returns:
-        The list of tuples combining polygon with associated mask.
+        The generator of tuples combining polygon with associated mask to iterate over.
     """
-    lines = file.readlines()
-
     # First line contains the number of polygon lines
-    polygon_count = int(lines[0])
-    polygons = []
+    polygon_count = int(file.readline())
 
     # The rest of lines have structure of white-space separated numeric values, where
     # first optional column has mask value (if this mode was requested by caller), then
     # the number of points in the polygon and after then all the x-coordinates of
     # polygon points followed by all the y-coordinates.
     for i in range(polygon_count):
-        line = lines[1 + i]
+        line = file.readline()
         parts = line.split()
+        # In case the line has an even number of cells, it means that the first cell
+        # contains the mask value.
+        mask_value_is_present = len(parts) % 2 == 0
         mask_value = 1
         index_base = 0
-        if support_mask_values:
+        if mask_value_is_present:
             mask_value = int(parts[0])
             index_base = 1
         pnt_count = int(parts[index_base])
@@ -44,6 +44,4 @@ def read_polygons_from_text_file(
             y = float(parts[index_base + 1 + pnt_count + j])
             points.append((x, y))
 
-        polygons.append((geometry.Polygon(points), mask_value))
-
-    return polygons
+        yield geometry.Polygon(points), mask_value
