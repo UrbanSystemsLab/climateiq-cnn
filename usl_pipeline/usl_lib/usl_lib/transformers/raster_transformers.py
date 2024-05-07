348fff248f1fd56bea05d664bda7f71cf4ce66d6
Roman Sutormin <rsutormin@google.com>
2024-05-07 21:41:34 +0000

    Review comments are addressed

--- usl_pipeline/usl_lib/usl_lib/transformers/raster_transformers.py
+++ usl_pipeline/usl_lib/usl_lib/transformers/raster_transformers.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 import numpy
 import numpy.typing as npt
-from shapely import geometry  # import Point, Polygon
+from shapely import geometry
 import geopandas as gpd
 
 from usl_lib.shared import geo_data
@@ -10,8 +10,8 @@ from usl_lib.shared import geo_data
 
 def fill_in_soil_classes_missing_values_from_nearest_polygons(
     elevation_header: geo_data.ElevationHeader,
-    green_areas_raster: npt.NDArray[numpy.float64],
-    soil_classes_raster: npt.NDArray[numpy.float64],
+    green_areas_raster: npt.NDArray[numpy.int_],
+    soil_classes_raster: npt.NDArray[numpy.int_],
     soil_classes_polygon_masks: list[Tuple[geometry.Polygon, int]],
 ) -> list[Tuple[geometry.Polygon, int]]:
     """Generates 1-cell polygons with nearest-neighbor soil classes for missing cells.
@@ -30,7 +30,7 @@ def fill_in_soil_classes_missing_values_from_nearest_polygons(
         The list of tuples containing polygons with associated soil classes for the
         cells in a raster with present green area but missing soil class.
     """
-    if len(soil_classes_polygon_masks) == 0:
+    if not soil_classes_polygon_masks:
         return []
 
     masks = [polygon_mask[1] for polygon_mask in soil_classes_polygon_masks]
@@ -42,19 +42,12 @@ def fill_in_soil_classes_missing_values_from_nearest_polygons(
     min_x = elevation_header.x_ll_corner
     max_y = elevation_header.y_ll_corner + elevation_header.row_count * cell_size
 
-    # Make common 3-dimensional array where both 2d arrays are 2 layers and 3-rd
-    # dimension is just 2-element array [<green-area-cell>, <soil-class-cell>]
-    # corresponding to the same place in the raster.
-    stacked_raster = numpy.dstack((green_areas_raster, soil_classes_raster))
-
-    # Reduce 3-rd dimension to an indicator that green area is present but soil class is
-    # 0 (missing value).
-    missing_soil_classes = numpy.apply_along_axis(
-        lambda gs: gs[0] == 1 and gs[1] == 0, 2, stacked_raster
-    )
+    # The following condition is an indicator that green area is present (value 1) but
+    # soil class is missing (value 0).
+    missing_soil_classes = (green_areas_raster == 1) & (soil_classes_raster == 0)
     soil_classes_corrections: list[Tuple[geometry.Polygon, int]] = []
-    missing_rows, missing_cols = numpy.where(missing_soil_classes == 1)
-    for row, col in zip(missing_rows.tolist(), missing_cols.tolist()):
+    missing_rows, missing_cols = numpy.where(missing_soil_classes)
+    for row, col in zip(missing_rows, missing_cols):
         # Calculate corners for the cell with missing soil class
         x1 = min_x + col * cell_size
         x2 = x1 + cell_size
