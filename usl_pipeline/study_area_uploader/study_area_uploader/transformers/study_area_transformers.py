e6b4ccb7e48efc3ca7c7a9fbf55457fc124fb572
Roman Sutormin <rsutormin@google.com>
2024-05-13 22:14:39 +0000

    Addressing comments from reviewer

--- usl_pipeline/study_area_uploader/study_area_uploader/transformers/study_area_transformers.py
+++ usl_pipeline/study_area_uploader/study_area_uploader/transformers/study_area_transformers.py
@@ -1,150 +1,49 @@
-import os
-import logging
 import pathlib
-from typing import Optional
+from typing import Iterable, Optional, Tuple
 
-from study_area_uploader.transformers import elevation_transformers
+from shapely import geometry
+
 from study_area_uploader.readers import polygon_readers as shape_readers
-from usl_lib.readers import elevation_readers
 from usl_lib.shared import geo_data
-from usl_lib.storage import file_names
 from usl_lib.transformers import polygon_transformers
-from usl_lib.writers import polygon_writers
 
 
-def _transform_shape_file(
+def transform_shape_file(
     input_shape_file_path: pathlib.Path | str,
-    output_polygon_file_path: pathlib.Path | str,
-    sub_area_bbox: Optional[geo_data.BoundingBox],
+    sub_area_bounding_box: Optional[geo_data.BoundingBox],
     target_crs: str,
     mask_value_feature_property: Optional[str] = None,
-) -> None:
-    """Writes a text file representation of the polygons inside `sub_area_bbox`."""
-    polygons = shape_readers.read_polygons_from_shape_file(
-        input_shape_file_path,
-        target_crs=target_crs,
-        mask_value_feature_property=mask_value_feature_property,
-    )
-    filtered_polygons = (
-        polygons
-        if sub_area_bbox is None
-        else list(
-            polygon_transformers.crop_polygons_to_sub_area(polygons, sub_area_bbox)
-        )
-    )
-    with open(output_polygon_file_path, "w") as output_polygon_file:
-        polygon_writers.write_polygons_to_text_file(
-            filtered_polygons,
-            output_polygon_file,
-            support_mask_values=(mask_value_feature_property is not None),
-        )
-
-
-def transform_study_area_files(
-    output_dir_path: pathlib.Path | str,
-    elevation_file_path: pathlib.Path | str,
-    sub_area_boundaries_shape_file_path: Optional[pathlib.Path | str] = None,
-    buildings_shape_file_path: Optional[pathlib.Path | str] = None,
-    green_areas_shape_file_path: Optional[pathlib.Path | str] = None,
-    soil_classes_shape_file_path: Optional[pathlib.Path | str] = None,
-    soil_classes_shape_file_property: Optional[str] = None,
-    elevation_output_file_name: str = file_names.ELEVATION_TIF,
-    boundaries_output_file_name: str = file_names.BOUNDARIES_TXT,
-    buildings_output_file_name: str = file_names.BUILDINGS_TXT,
-    green_areas_output_file_name: str = file_names.GREEN_AREAS_TXT,
-    soil_classes_output_file_name: str = file_names.SOIL_CLASSES_TXT,
-) -> pathlib.Path:
-    """Function prepares study area input files in a format expected by the pipeline.
-
-    Most of the files (except elevation one) are optional. If sub-area boundaries file
-    is defined all input files are cropped to the bounding box calculated for the
-    boundaries. Output files are created in the output directory (except for the case of
-    elevation file with no need to crop it, in this case the original file should be
-    used).
+    skip_zero_masks: bool = True,
+) -> Iterable[Tuple[geometry.Polygon, int]]:
+    """Reads, filters and transforms polygons from shape-file with optional cropping.
 
     Args:
-        output_dir_path: Directory for output files.
-        elevation_file_path: Path to input elevation data.
-        sub_area_boundaries_shape_file_path: Optional shape-file with boundaries that
-            is used to crop study area.
-        buildings_shape_file_path: Optional shape-file with buildings.
-        green_areas_shape_file_path: Optional shape-file with green areas.
-        soil_classes_shape_file_path: Optional shape-file with soil classes
-        soil_classes_shape_file_property: Optional property name in soil classes shape
-            file that defines soil class values.
-        elevation_output_file_name: Default elevation output file name.
-        boundaries_output_file_name: Default boundaries output file name.
-        buildings_output_file_name: Default buildings output file name.
-        green_areas_output_file_name: Default green areas output file name.
-        soil_classes_output_file_name: Default soil classes output file name.
+        input_shape_file_path: Path to a shape file to read from.
+        sub_area_bounding_box: Optional bounding box that is used to crop polygon area.
+        target_crs: CRS that polygon coordinates should be translated to (typically
+            comes from elevation data header).
+        mask_value_feature_property: Optional shape feature property to load mask from.
+            In case this property is not defined by the caller, mask value 1 is used.
+        skip_zero_masks: Indicator that polygons associated with 0 masks should be
+            filtered out (only has effect when mask_value_feature_property is defined).
 
     Returns:
-        Output file path to elevation data (can point to input file path in case no
-        changes are required for it).
+        Iterator of polygons with associated masks.
     """
-    with pathlib.Path(elevation_file_path).open("rb") as input_file:
-        elevation_header = elevation_readers.read_from_geotiff(
-            input_file, header_only=True
-        ).header
-    crs = elevation_header.crs.to_string()
-
-    output_dir = pathlib.Path(output_dir_path)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    sub_area_bbox: Optional[geo_data.BoundingBox] = None
-    if sub_area_boundaries_shape_file_path is not None:
-        logging.info("Preparing Boundaries...")
-        boundaries_polygons = shape_readers.read_polygons_from_shape_file(
-            sub_area_boundaries_shape_file_path, target_crs=crs
+    polygons_iterator = (
+        p
+        for p in shape_readers.read_polygons_from_shape_file(
+            input_shape_file_path,
+            target_crs=target_crs,
+            mask_value_feature_property=mask_value_feature_property,
         )
-        boundaries_file_path = output_dir / boundaries_output_file_name
-        with boundaries_file_path.open("w") as output_file:
-            polygon_writers.write_polygons_to_text_file(
-                boundaries_polygons, output_file
-            )
-        sub_area_bbox = polygon_transformers.get_bounding_box_for_boundaries(
-            p[0] for p in boundaries_polygons
+    )
+    if mask_value_feature_property is not None and skip_zero_masks:
+        polygons_iterator = (p for p in polygons_iterator if p[1] != 0)
+    return (
+        polygons_iterator
+        if sub_area_bounding_box is None
+        else polygon_transformers.crop_polygons_to_sub_area(
+            polygons_iterator, sub_area_bounding_box
         )
-
-    if sub_area_bbox is not None:
-        logging.info("Preparing Elevation data...")
-        output_elevation_file = output_dir / elevation_output_file_name
-        elevation_transformers.crop_geotiff_to_sub_area(
-            elevation_file_path, output_elevation_file, sub_area_bbox
-        )
-    else:
-        output_elevation_file = pathlib.Path(elevation_file_path)
-
-    if buildings_shape_file_path is not None:
-        logging.info("Preparing Buildings...")
-        sub_area_buildings_file = output_dir / buildings_output_file_name
-        _transform_shape_file(
-            buildings_shape_file_path,
-            sub_area_buildings_file,
-            sub_area_bbox,
-            target_crs=crs,
-        )
-
-    if green_areas_shape_file_path is not None:
-        logging.info("Preparing Green Areas...")
-        output_green_areas_file = output_dir / green_areas_output_file_name
-        _transform_shape_file(
-            green_areas_shape_file_path,
-            output_green_areas_file,
-            sub_area_bbox,
-            target_crs=crs,
-        )
-
-    if soil_classes_shape_file_path is not None:
-        logging.info("Preparing Soil Classes...")
-        output_soil_classes_file = output_dir / soil_classes_output_file_name
-        _transform_shape_file(
-            soil_classes_shape_file_path,
-            output_soil_classes_file,
-            sub_area_bbox,
-            target_crs=crs,
-            mask_value_feature_property=soil_classes_shape_file_property,
-        )
-
-    return output_elevation_file
+    )
--- usl_pipeline/study_area_uploader/tests/transformers/test_study_area_transformers.py
+++ usl_pipeline/study_area_uploader/tests/transformers/test_study_area_transformers.py
@@ -3,33 +3,10 @@ import tempfile
 from typing import Tuple
 
 import fiona
-import numpy
-from numpy import testing
-import rasterio
 from shapely import geometry
 
 from study_area_uploader.transformers import study_area_transformers
-from usl_lib.readers import elevation_readers, polygon_readers
 from usl_lib.shared import geo_data
-
-
-def prepare_elevation_data(
-    elevation_file_path: pathlib.Path, data: list[list[float]]
-) -> None:
-    with rasterio.open(
-        str(elevation_file_path),
-        "w",
-        driver="GTiff",
-        dtype=rasterio.float32,
-        nodata=0.0,
-        width=5,
-        height=5,
-        count=1,
-        crs=rasterio.CRS.from_epsg(32618),
-        # The lower-left corner in the following transform is (0, 0) and cell_size is 1.
-        transform=rasterio.Affine(1.0, 0.0, 0.0, 0.0, -1.0, 5.0),
-    ) as raster:
-        raster.write(numpy.array(data).astype(rasterio.float32), 1)
 
 
 def polygon_points(polygon: geometry.Polygon) -> list[Tuple[float, float]]:
@@ -69,134 +46,55 @@ def bbox_polygon(x1: float, y1: float, x2: float, y2: float) -> geometry.Polygon
     return geometry.Polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)])
 
 
-def test_transform_study_area_files_crop_boundaries_no_soil_classes():
-    with (
-        tempfile.TemporaryDirectory() as temp_dir,
-        tempfile.TemporaryDirectory() as temp_dir2,
-    ):
+def test_transform_shape_file_no_polygon_masks():
+    with (tempfile.TemporaryDirectory() as temp_dir,):
         input_dir = pathlib.Path(temp_dir)
-        output_dir = pathlib.Path(temp_dir2)
-        elevation_input_file_path = input_dir / "elevation_input.tif"
-        prepare_elevation_data(
-            elevation_input_file_path,
-            [
-                [0.0, 1.0, 2.0, 3.0, 4.0],
-                [5.0, 6.0, 7.0, 8.0, 9.0],
-                [10.0, 11.0, 12.0, 13.0, 14.0],
-                [15.0, 16.0, 17.0, 18.0, 19.0],
-                [20.0, 21.0, 22.0, 23.0, 24.0],
-            ],
-        )
-
-        boundaries_input_file_path = input_dir / "boundaries.shp"
-        prepare_shape_file(boundaries_input_file_path, [(bbox_polygon(1, 1, 3, 3), 1)])
-
         buildings_input_file_path = input_dir / "buildings.shp"
-        prepare_shape_file(buildings_input_file_path, [(bbox_polygon(2, 1, 4, 3), 1)])
-
-        green_areas_input_file_path = input_dir / "green_areas.shp"
-        prepare_shape_file(green_areas_input_file_path, [(bbox_polygon(1, 0, 3, 2), 1)])
-
-        output_file_path = study_area_transformers.transform_study_area_files(
-            output_dir,
-            elevation_input_file_path,
-            sub_area_boundaries_shape_file_path=boundaries_input_file_path,
-            buildings_shape_file_path=buildings_input_file_path,
-            green_areas_shape_file_path=green_areas_input_file_path,
-        )
-
-        assert output_file_path == output_dir / "elevation.tif"
-
-        with output_file_path.open("rb") as input_file:
-            elevation = elevation_readers.read_from_geotiff(input_file)
-        assert elevation.header == geo_data.ElevationHeader(
-            col_count=4,
-            row_count=4,
-            x_ll_corner=0.0,
-            y_ll_corner=0.0,
-            cell_size=1.0,
-            nodata_value=0.0,
-            crs=rasterio.CRS({"init": "EPSG:32618"}),
-        )
-        testing.assert_array_equal(
-            elevation.data,
+        prepare_shape_file(
+            buildings_input_file_path,
             [
-                [5.0, 6.0, 7.0, 8.0],
-                [10.0, 11.0, 12.0, 13.0],
-                [15.0, 16.0, 17.0, 18.0],
-                [20.0, 21.0, 22.0, 23.0],
+                (bbox_polygon(2, 1, 4, 3), 1),
             ],
         )
 
-        with open(output_dir / "buildings.txt", "r") as input_file:
-            cropped_buildings = list(
-                polygon_readers.read_polygons_from_text_file(input_file)
+        cropped_buildings = list(
+            study_area_transformers.transform_shape_file(
+                buildings_input_file_path,
+                geo_data.BoundingBox(min_x=1, min_y=1, max_x=3, max_y=3),
+                "EPSG:32618",
             )
-            assert len(cropped_buildings) == 1
-            assert cropped_buildings[0][0].equals(bbox_polygon(2, 1, 3, 3))
+        )
 
-        with open(output_dir / "green_areas.txt", "r") as input_file:
-            cropped_buildings = list(
-                polygon_readers.read_polygons_from_text_file(input_file)
-            )
-            assert len(cropped_buildings) == 1
-            assert cropped_buildings[0][0].equals(bbox_polygon(1, 1, 3, 2))
+        assert len(cropped_buildings) == 1
+        assert cropped_buildings[0][0].equals(bbox_polygon(2, 1, 3, 3))
 
 
-def test_transform_study_area_files_no_crop_boundaries_soil_classes_only():
-    input_elevation_data = [
-        [0.0, 1.0, 2.0, 3.0, 4.0],
-        [5.0, 6.0, 7.0, 8.0, 9.0],
-        [10.0, 11.0, 12.0, 13.0, 14.0],
-        [15.0, 16.0, 17.0, 18.0, 19.0],
-        [20.0, 21.0, 22.0, 23.0, 24.0],
-    ]
-
-    with (
-        tempfile.TemporaryDirectory() as temp_dir,
-        tempfile.TemporaryDirectory() as temp_dir2,
-    ):
+def test_transform_shape_file_with_polygon_masks():
+    with (tempfile.TemporaryDirectory() as temp_dir,):
         input_dir = pathlib.Path(temp_dir)
-        output_dir = pathlib.Path(temp_dir2)
-        elevation_input_file_path = input_dir / "elevation_input.tif"
-        prepare_elevation_data(elevation_input_file_path, input_elevation_data)
-
         soil_classes_input_file_path = input_dir / "soil_classes.shp"
         soil_class_prop = "soil_class"
         prepare_shape_file(
             soil_classes_input_file_path,
-            [(bbox_polygon(2, 1, 4, 3), 8), (bbox_polygon(1, 0, 3, 2), 9)],
+            [
+                (bbox_polygon(0, 0, 5, 5), 0),  # It has 0 mask and will be skipped
+                (bbox_polygon(2, 1, 4, 3), 8),
+                (bbox_polygon(1, 0, 3, 2), 9),
+            ],
             soil_class_prop=soil_class_prop,
         )
 
-        output_file_path = study_area_transformers.transform_study_area_files(
-            output_dir,
-            elevation_input_file_path,
-            soil_classes_shape_file_path=soil_classes_input_file_path,
-            soil_classes_shape_file_property=soil_class_prop,
-        )
-
-        assert output_file_path == elevation_input_file_path
-
-        with output_file_path.open("rb") as input_file:
-            elevation = elevation_readers.read_from_geotiff(input_file)
-        assert elevation.header == geo_data.ElevationHeader(
-            col_count=5,
-            row_count=5,
-            x_ll_corner=0.0,
-            y_ll_corner=0.0,
-            cell_size=1.0,
-            nodata_value=0.0,
-            crs=rasterio.CRS({"init": "EPSG:32618"}),
-        )
-        testing.assert_array_equal(elevation.data, input_elevation_data)
-
-        with open(output_dir / "soil_classes.txt", "r") as input_file:
-            soil_classes = list(
-                polygon_readers.read_polygons_from_text_file(input_file)
+        soil_classes = list(
+            study_area_transformers.transform_shape_file(
+                soil_classes_input_file_path,
+                None,
+                "EPSG:32618",
+                mask_value_feature_property=soil_class_prop,
             )
-            assert len(soil_classes) == 2
-            assert soil_classes[0][0].equals(bbox_polygon(2, 1, 4, 3))
-            assert soil_classes[0][1] == 8
-            assert soil_classes[1][0].equals(bbox_polygon(1, 0, 3, 2))
-            assert soil_classes[1][1] == 9
+        )
+
+        assert len(soil_classes) == 2
+        assert soil_classes[0][0].equals(bbox_polygon(2, 1, 4, 3))
+        assert soil_classes[0][1] == 8
+        assert soil_classes[1][0].equals(bbox_polygon(1, 0, 3, 2))
+        assert soil_classes[1][1] == 9
