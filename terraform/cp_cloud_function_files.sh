b83872663f09e04ffbd2511bacc22ec4180a0eda
Walt Askew <waltaskew@google.com>
2024-06-20 10:06:28 -0700

    Remove Need to Run cp_cloud_function_files.sh (#80)
    - Move the full creation of the cloud function source .zip file into
      terraform

--- terraform/cp_cloud_function_files.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-mkdir -p "$(dirname $0)/modules/data_pipeline/files/cloud_function_source"
-rsync "$(dirname $0)/../usl_pipeline/cloud_functions/main.py" "$(dirname $0)/modules/data_pipeline/files/cloud_function_source/"
-rsync "$(dirname $0)/../usl_pipeline/cloud_functions/requirements.txt" "$(dirname $0)/modules/data_pipeline/files/cloud_function_source/"
-rsync -r --include '*.py' --exclude '*.pyc' "$(dirname $0)/../usl_pipeline/usl_lib/usl_lib" "$(dirname $0)/modules/data_pipeline/files/cloud_function_source/"
