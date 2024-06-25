4a66c22cabc870d07a99c675e5e3f362cfdba5b4
prashantkul <pskulkarni@google.com>
2024-06-24 21:44:08 -0700

    Created a simple training function

--- usl_models/usl_models/flood_ml/train_model.py
+++ usl_models/usl_models/flood_ml/train_model.py
@@ -41,67 +41,67 @@ Returns:
 """
 
 
-def train_model(
-    model_dir: str,
-    batch_size: int,
-    lstm_units: int,
-    lstm_kernel_size: int,
-    lstm_dropout: float,
-    lstm_recurrent_dropout: float,
-    epochs: int,
-):
+# def train_model(
+#     model_dir: str,
+#     batch_size: int,
+#     lstm_units: int,
+#     lstm_kernel_size: int,
+#     lstm_dropout: float,
+#     lstm_recurrent_dropout: float,
+#     epochs: int,
+# ):
 
-    # Create FloodModelParams from hyperparameters
-    model_params = FloodModelParams(
-        batch_size=batch_size,
-        lstm_units=lstm_units,
-        lstm_kernel_size=lstm_kernel_size,
-        lstm_dropout=lstm_dropout,
-        lstm_recurrent_dropout=lstm_recurrent_dropout,
-        epochs=epochs,
-    )
-    model_history = []
+#     # Create FloodModelParams from hyperparameters
+#     model_params = FloodModelParams(
+#         batch_size=batch_size,
+#         lstm_units=lstm_units,
+#         lstm_kernel_size=lstm_kernel_size,
+#         lstm_dropout=lstm_dropout,
+#         lstm_recurrent_dropout=lstm_recurrent_dropout,
+#         epochs=epochs,
+#     )
+#     model_history = []
 
-    # Instantiate FloodModel class
-    model = FloodModel(model_params)
-    # model = model.compile(tf.keras.optimizers.Adam(learning_rate=learning_rate))
+#     # Instantiate FloodModel class
+#     model = FloodModel(model_params)
+#     # model = model.compile(tf.keras.optimizers.Adam(learning_rate=learning_rate))
 
-    class_label = GenerateFeatureLabelChunks()
-    sim_names = class_label._get_sim_names_from_metastore()
-    print(sim_names)
-    batch_size = 32
+#     class_label = GenerateFeatureLabelChunks()
+#     sim_names = class_label._get_sim_names_from_metastore()
+#     print(sim_names)
+#     batch_size = 32
 
-    generator = IncrementalTrainDataGenerator(
-        batch_size=batch_size,
-    )
+#     generator = IncrementalTrainDataGenerator(
+#         batch_size=batch_size,
+#     )
 
-    # compare feature and label chunks
-    for sim_name in sim_names:
-        print(f"Comparing feature and label chunks for {sim_name}")
-        label_compare = class_label.compare_study_area_sim_chunks(sim_name)
-        if label_compare:
-            print("Feature and label chunks are equal for", sim_name)
-            print("")
-        else:
-            print("Feature and label chunks are not equal")
-            sim_names.remove(sim_name)
+#     # compare feature and label chunks
+#     for sim_name in sim_names:
+#         print(f"Comparing feature and label chunks for {sim_name}")
+#         label_compare = class_label.compare_study_area_sim_chunks(sim_name)
+#         if label_compare:
+#             print("Feature and label chunks are equal for", sim_name)
+#             print("")
+#         else:
+#             print("Feature and label chunks are not equal")
+#             sim_names.remove(sim_name)
 
-    print("**** Length of sim_names: ", len(sim_names))
+#     print("**** Length of sim_names: ", len(sim_names))
 
-    # Get the next batch of data
-    if len(sim_names) > 0:
-        data = generator.get_next_batch(sim_names, batch_size)
-        print("\n")
-        print("Dataset generation completed, will hand over to model training..")
-        print("\n")
-        print("Training model")
-        model_history.append(model.train(data))
-        print("Training complete")
+#     # Get the next batch of data
+#     if len(sim_names) > 0:
+#         data = generator.get_next_batch(sim_names, batch_size)
+#         print("\n")
+#         print("Dataset generation completed, will hand over to model training..")
+#         print("\n")
+#         print("Training model")
+#         model_history.append(model.train(data))
+#         print("Training complete")
 
-    else:
-        print("No valid sims found for training data generation")
+#     else:
+#         print("No valid sims found for training data generation")
 
-    return model_history
+#     return model_history
 
 
 def verify_labels_shape(flood_model_data_list):
@@ -124,78 +124,48 @@ def verify_labels_shape(flood_model_data_list):
         print("-" * 20)
 
 
-def main():
+def simple_training(
+    model_dir="gs://usl_models_bucket/flood_model",
+    batch_size=32,
+    lstm_units=128,
+    lstm_kernel_size=3,
+    lstm_dropout=0.2,
+    lstm_recurrent_dropout=0.2,
+    epochs=1,
+):
 
-    print(tf.__version__)
+    # Create FloodModelParams from hyperparameters
+    model_params = FloodModelParams(
+        batch_size=batch_size,
+        lstm_units=lstm_units,
+        lstm_kernel_size=lstm_kernel_size,
+        lstm_dropout=lstm_dropout,
+        lstm_recurrent_dropout=lstm_recurrent_dropout,
+        epochs=epochs,
+    )
+    model_history = []
 
-    # model_histories = train_model(
-    #     model_dir="gs://usl_models_bucket/flood_model",
-    #     batch_size=32,
-    #     lstm_units=128,
-    #     lstm_kernel_size=3,
-    #     lstm_dropout=0.2,
-    #     lstm_recurrent_dropout=0.2,
-    #     epochs=1,
-    # )
+    # Instantiate FloodModel class
+    model = FloodModel(model_params)
+    # model = model.compile(tf.keras.optimizers.Adam(learning_rate=learning_rate))
 
-    # for history in model_histories:
-    #     print(history.history)
-
-    class_label = GenerateFeatureLabelChunks()
-    sim_names = class_label._get_sim_names_from_metastore()
-    print(sim_names)
-    batch_size = 32
+    sim_names = ["Manhattan-config_v1%2FRainfall_Data_2.txt"]
 
     generator = IncrementalTrainDataGenerator(
         batch_size=batch_size,
     )
 
-    sim_names = [
-        "Manhattan-config_v1%2FRainfall_Data_2.txt",
-        "Manhattan-config_v1%2FRainfall_Data_13.txt",
-        "Manhattan-config_v1%2FRainfall_Data_14.txt",
-        "Manhattan-config_v1%2FRainfall_Data_15.txt",
-        "Manhattan-config_v1%2FRainfall_Data_17.txt",
-    ]
-    # compare feature and label chunks
-    # for sim_name in sim_names:
-    #     print(f"Comparing feature and label chunks for {sim_name}")
-    #     label_compare = class_label.compare_study_area_sim_chunks(sim_name)
-    #     if label_compare:
-    #         print("Feature and label chunks are equal for", sim_name)
-    #         print("")
-    #     else:
-    #         print("Feature and label chunks are not equal")
-    #         sim_names.remove(sim_name)
-
-    print("**** Length of sim_names: ", len(sim_names))
-    print("Printing valid sims in metastore")
-    for sim_name in sim_names:
-        print(f"Index: {sim_names.index(sim_name)} , Sim name: {sim_name}")
-        print("")
-
-    # first_sim_name = sim_names[0]
-
-    # slice sim_names to get the first two elements
-    sim_names_2 = sim_names[:2]
-
-    # # download npy label chunks locally
-    # generator.download_numpy_files(sim_names_2, "label")
+    # download npy label chunks locally
+    generator.download_numpy_files(sim_names, "label")
 
     # download npy feature chunks locally
-    generator.download_numpy_files(sim_names_2, "feature")
+    generator.download_numpy_files(sim_names, "feature")
 
-    # # download npy temporal chunks locally
-    # generator.download_numpy_files(sim_names_2, "temporal")
-
-    # print("Printing valid sims in metastore"))
-
-    for sim_name in sim_names_2:
-        print(f"Index: {sim_names_2.index(sim_name)} , Sim name: {sim_name}")
-        print("")
+    # download npy temporal chunks locally
+    generator.download_numpy_files(sim_names, "temporal")
 
     # Get the next batch of data for one simulation
-    data_batch = generator.get_next_batch(sim_names_2, batch_size)
+    data_batch = generator.get_next_batch(sim_names, batch_size)
 
     print("\n")
 
@@ -203,36 +173,136 @@ def main():
     print("Data batch size:", len(data_batch))
     print("\n")
 
-    # Print the shapes of the tensors in the batch
-    for data in data_batch:
-        # Take the first element from tf dataset iterator and print shape of each tensors
-        print("_" * 10, "\n\n")
-        print("Temporal data shape:", next(iter(data.temporal.take(1))).shape)
-        print("_" * 10, "\n\n")
-        print("Labels shape:", next(iter(data.labels.take(1))).shape)
-        print("_" * 10, "\n\n")
-        print(
-            "geospatial Feature tensor shape:",
-            next(iter(data.geospatial.take(1))).shape,
-        )
-        print("_" * 10, "\n\n")
-        print("Rainfall duration:", data.storm_duration)
-        print("")
-        print("-" * 20)
+    print("Dataset generation completed, will hand over to model training..")
+    print("\n")
+    print("#######  Training model ##########")
+    model_history.append(model.train(data_batch))
+    print("Training complete")
+    return model_history
 
-        # # Validate the shape of the labels tensor
-        # first_label = next(iter(data.take(1)))[0]  # Get the first label batch
-        # expected_label_shape = list(first_label.shape)
-        # expected_label_shape[-1] = rainfall_duration
+def set_tf_gpu():
+    print(tf.__version__)
+    # Check if TensorFlow was built with CUDA
+    print("TensorFlow built with CUDA support:", tf.test.is_built_with_cuda())
 
-        # # Labels must match the storm duration.
-        # assert first_label.shape[-1] == rainfall_duration, (  # Compare the shape of the first label tensor
-        #     "Provided labels are inconsistent with storm duration. "
-        #     f"Labels are expected to have shape {expected_label_shape}. "
-        #     f"Actual shape: {first_label.shape}."  # Use first_label.shape
-        # )
-        # print("Labels are consistent with storm duration.")
-        # print("-" * 20)
+    # List available GPUs
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        print("Num GPUs Available: ", len(gpus))
+    else:
+        print("No GPUs found")
+
+    # Configure TensorFlow for GPU usage
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Use only the first GPU (T4 in your case)
+            tf.config.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logicalexpand_more GPUs")
+        except RuntimeError as e:
+            print(e)  # Handle potent
+
+
+def main():
+
+    set_tf_gpu()
+    model_histories = simple_training()
+
+    for history in model_histories:
+        print(history.history)
+
+
+        
+    # generator = IncrementalTrainDataGenerator(
+    #     batch_size=batch_size,
+    # )
+
+    # sim_names = [
+    #     "Manhattan-config_v1%2FRainfall_Data_2.txt",
+    #     "Manhattan-config_v1%2FRainfall_Data_13.txt",
+    #     "Manhattan-config_v1%2FRainfall_Data_14.txt",
+    #     "Manhattan-config_v1%2FRainfall_Data_15.txt",
+    #     "Manhattan-config_v1%2FRainfall_Data_17.txt",
+    # ]
+    # # compare feature and label chunks
+    # # for sim_name in sim_names:
+    # #     print(f"Comparing feature and label chunks for {sim_name}")
+    # #     label_compare = class_label.compare_study_area_sim_chunks(sim_name)
+    # #     if label_compare:
+    # #         print("Feature and label chunks are equal for", sim_name)
+    # #         print("")
+    # #     else:
+    # #         print("Feature and label chunks are not equal")
+    # #         sim_names.remove(sim_name)
+
+    # print("**** Length of sim_names: ", len(sim_names))
+    # print("Printing valid sims in metastore")
+    # for sim_name in sim_names:
+    #     print(f"Index: {sim_names.index(sim_name)} , Sim name: {sim_name}")
+    #     print("")
+
+    # # first_sim_name = sim_names[0]
+
+    # # slice sim_names to get the first two elements
+    # sim_names_2 = sim_names[:2]
+
+    # # # download npy label chunks locally
+    # # generator.download_numpy_files(sim_names_2, "label")
+
+    # # download npy feature chunks locally
+    # generator.download_numpy_files(sim_names_2, "feature")
+
+    # # # download npy temporal chunks locally
+    # # generator.download_numpy_files(sim_names_2, "temporal")
+
+    # # print("Printing valid sims in metastore"))
+
+    # for sim_name in sim_names_2:
+    #     print(f"Index: {sim_names_2.index(sim_name)} , Sim name: {sim_name}")
+    #     print("")
+
+    # # Get the next batch of data for one simulation
+    # data_batch = generator.get_next_batch(sim_names_2, batch_size)
+
+    # print("\n")
+
+    # # print size of data_batch
+    # print("Data batch size:", len(data_batch))
+    # print("\n")
+
+    # # Print the shapes of the tensors in the batch
+    # for data in data_batch:
+    #     # Take the first element from tf dataset iterator and print shape of each tensors
+    #     print("_" * 10, "\n\n")
+    #     print("Temporal data shape:", next(iter(data.temporal.take(1))).shape)
+    #     print("_" * 10, "\n\n")
+    #     print("Labels shape:", next(iter(data.labels.take(1))).shape)
+    #     print("_" * 10, "\n\n")
+    #     print(
+    #         "geospatial Feature tensor shape:",
+    #         next(iter(data.geospatial.take(1))).shape,
+    #     )
+    #     print("_" * 10, "\n\n")
+    #     print("Rainfall duration:", data.storm_duration)
+    #     print("")
+    #     print("-" * 20)
+
+    #     # # Validate the shape of the labels tensor
+    #     # first_label = next(iter(data.take(1)))[0]  # Get the first label batch
+    #     # expected_label_shape = list(first_label.shape)
+    #     # expected_label_shape[-1] = rainfall_duration
+
+    #     # # Labels must match the storm duration.
+    #     # assert first_label.shape[-1] == rainfall_duration, (  # Compare the shape of the first label tensor
+    #     #     "Provided labels are inconsistent with storm duration. "
+    #     #     f"Labels are expected to have shape {expected_label_shape}. "
+    #     #     f"Actual shape: {first_label.shape}."  # Use first_label.shape
+    #     # )
+    #     # print("Labels are consistent with storm duration.")
+    #     # print("-" * 20)
+
+
 
 
 if __name__ == "__main__":
