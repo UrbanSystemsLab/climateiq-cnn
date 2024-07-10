77764ceff79763a5e4c6f1fbfa8a295fde466128
Tatiana Wu <tatianawu@google.com>
2024-07-10 14:18:34 -0400

    Implement time step pair splitting

--- usl_models/usl_models/atmo_ml/data_utils.py
+++ usl_models/usl_models/atmo_ml/data_utils.py
@@ -30,3 +30,31 @@ def boundary_pairs(data: tf.Tensor) -> tf.Tensor:
     pairs = stride_tricks.sliding_window_view(data, 2, axis=1)
     pairs = tf.reshape(pairs, pairs.shape[:-2] + (-1,))
     return pairs
+
+
+def split_time_step_pairs(data: tf.Tensor) -> tf.Tensor:
+    """Splits a tensor into two time steps.
+
+    Takes a time sequence of T paired tensors and returns 2T single tensors.
+    This function follows the work done by boundary_pairs, which groups tensors
+    tensors up, and splits them up again prior to retrieving the final output.
+
+        Input: [[T0, T1], [T2, T3], [T4, T5]]
+        Output: [T0, T1, T2, T3, T4, T5]
+
+    For more details about this function and boundary_pairs, see
+    https://www.notion.so/climate-iq/AtmoML-Architecture-Proposals-and-Design-c00d0e54265c4bb8a72ce01fd475f116?pvs=4#455d785c174a482c90a78efde380adf3.
+
+    Args:
+        data: A time sequence of paired tensors of shape [B, T, H, W, 2C].
+
+    Returns:
+        A time sequence of single tensors of shape [B, 2T, H, W, C].
+    """
+    # We need to permute the tensor to place the time and channel axes together
+    # during the reshape, in order to avoid disturbing the spatial dimensions.
+    permuted = tf.transpose(data, (0, 2, 3, 1, 4))
+    shape = permuted.shape[:3] + (permuted.shape[-2] * 2, permuted.shape[-1] // 2)
+    reshaped = tf.reshape(permuted, shape)
+    output = tf.transpose(reshaped, (0, 3, 1, 2, 4))
+    return output
