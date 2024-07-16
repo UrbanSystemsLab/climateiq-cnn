fc1d43dd633d69d0860d97c7529330022cdf7dd4
Josiah Putman <joshikatsu@gmail.com>
2024-07-16 19:02:01 +0000

    Improve get_temporal_window test

--- usl_models/tests/flood_ml/flood_model_test.py
+++ usl_models/tests/flood_ml/flood_model_test.py
@@ -162,7 +162,7 @@ def test_model_checkpoint():
 def test_get_temporal_window():
     """Tests copmuting the temporal window."""
     B, T_MAX, M = 1, 8, 6
-    temporal = tf.constant([[[t] * M for t in range(T_MAX)]], dtype=tf.float32)
+    temporal = tf.constant([[[t + 1] * M for t in range(T_MAX)]], dtype=tf.float32)
 
     t = 3  # Timestep
     n = 5  # Window size
@@ -172,9 +172,9 @@ def test_get_temporal_window():
         [
             np.zeros((M)),
             np.zeros((M)),
-            np.zeros((M)),
             [1, 1, 1, 1, 1, 1],
             [2, 2, 2, 2, 2, 2],
+            [3, 3, 3, 3, 3, 3],
         ],
     ]
     np.testing.assert_array_equal(actual, expected)
