becaad3daa67a1dad88ec771605d56059c3654b9
Tatiana Wu <tatianawu@google.com>
2024-07-23 17:11:12 -0700

    Change eval metrics to take tensors

--- usl_models/tests/flood_ml/flood_eval_test.py
+++ usl_models/tests/flood_ml/flood_eval_test.py
@@ -1,37 +1,45 @@
 """Tests for FloodML evaluation metrics."""
 
 import numpy as np
+import tensorflow as tf
 
 from usl_models.flood_ml import eval
 
 
 def test_spatial_mae():
     """Tests spatial MAE calculation."""
-    label = np.arange(18, dtype=np.float64).reshape((2, 3, 3))
+    label = tf.reshape(tf.range(18, dtype=tf.float64), (2, 3, 3))
     pred = label * 1.01
 
     actual = eval.spatial_mae(pred, label)
-    expected = [
-        [0.045, 0.055, 0.065],
-        [0.075, 0.085, 0.095],
-        [0.105, 0.115, 0.125],
-    ]
-
+    expected = tf.convert_to_tensor(
+        [
+            [0.045, 0.055, 0.065],
+            [0.075, 0.085, 0.095],
+            [0.105, 0.115, 0.125],
+        ],
+        dtype=tf.float64,
+    )
     np.testing.assert_allclose(actual, expected, rtol=1e-6)
 
 
-def test_time_series_errors():
-    """Tests time series error calculations."""
-    label = np.arange(72, dtype=np.float32).reshape((2, 4, 3, 3))
-    error = np.arange(8).reshape((2, 4)) / 10
-    pred = label + np.tile(np.expand_dims(error, (2, 3)), (1, 1, 3, 3))
+def test_temporal_mae():
+    """Tests temporal MAE calculation."""
+    label = tf.reshape(tf.range(72, dtype=tf.float64), (2, 4, 3, 3))
+    error = tf.reshape(tf.range(8, dtype=tf.float64), (2, 4)) / 10
+    pred = label + tf.tile(error[:, :, tf.newaxis, tf.newaxis], (1, 1, 3, 3))
 
-    actual = eval.time_series_errors(pred, label)
-    expected = {
-        "mae": [0.2, 0.3, 0.4, 0.5],
-        "rmse": np.sqrt([0.08, 0.13, 0.2, 0.29]),
-    }
+    actual = eval.temporal_mae(pred, label)
+    expected = tf.convert_to_tensor([0.2, 0.3, 0.4, 0.5], dtype=tf.float64)
+    np.testing.assert_allclose(actual, expected, rtol=1e-6)
 
-    assert actual.keys() == expected.keys()
-    for k, v in actual.items():
-        np.testing.assert_allclose(v, expected[k], rtol=1e-6)
+
+def test_temporal_rmse():
+    """Tests temporal RMSE calculation."""
+    label = tf.reshape(tf.range(72, dtype=tf.float64), (2, 4, 3, 3))
+    error = tf.reshape(tf.range(8, dtype=tf.float64), (2, 4)) / 10
+    pred = label + tf.tile(error[:, :, tf.newaxis, tf.newaxis], (1, 1, 3, 3))
+
+    actual = eval.temporal_rmse(pred, label)
+    expected = tf.sqrt([0.08, 0.13, 0.2, 0.29])
+    np.testing.assert_allclose(actual, expected, rtol=1e-6)
