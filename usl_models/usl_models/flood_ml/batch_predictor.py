ccd97f5cb4590725adab0149e1f83be8c9a86f02
Josiah Putman <joshikatsu@gmail.com>
2024-07-30 17:31:30 +0000

    Improve docs for BatchPredictor

--- usl_models/usl_models/flood_ml/batch_predictor.py
+++ usl_models/usl_models/flood_ml/batch_predictor.py
@@ -1,14 +1,17 @@
-"""Utilities for running batch predictions on files stored in GCS."""
+"""Utilities for running batch predictions on files stored in GCS.
 
-import json
-import urllib.parse
-import logging
+See `BatchPredictor` for a full description of the approach.
+"""
+
 import dataclasses
-import tensorflow as tf
+import json
+import logging
+import urllib.parse
 
 from google.cloud import firestore  # type:ignore[attr-defined]
 from google.cloud import storage  # type:ignore[attr-defined]
 import numpy as np
+import tensorflow as tf
 
 from usl_models.flood_ml import model
 from usl_models.flood_ml import prediction_dataset
@@ -20,6 +23,18 @@ class BatchPredictor:
 
     Ouputs data based on the specified `output_bucket`, `model_id`,
     and `run_id`.
+
+    To use this class to run batch prediction on "NYC" study area:
+    ```py
+    predictor = BatchPredictor(
+        model=FloodModel.from_checkpoint("gs://my/model"),
+        ...)
+    predictor.predict_and_save_scenario(
+        study_area_id="NYC",
+        config_id="config_v1/Rainfall_Data_3.txt",
+        scenario_id="flood_depth_1y",
+    )
+    ```
     """
 
     db: firestore.Client
@@ -28,36 +43,7 @@ class BatchPredictor:
     model_id: str
     model: model.FloodModel
     run_id: str
-    batch_size: int = 1
-
-    def get_prediction_npy_path(
-        self, study_area_id: str, config_id: str, chunk_id: str
-    ) -> str:
-        """Returns the GCS filepath for a prediction npy file."""
-        return (
-            f"{self.run_id}/flood/{self.model_id}/{study_area_id}"
-            + f"/{config_id}/{chunk_id}"
-        )
-
-    def save_prediction_npy(
-        self, study_area_id: str, config_id: str, chunk_id: str, prediction: np.ndarray
-    ) -> None:
-        """Saves a prediction npy file to GCS."""
-        path = self.get_prediction_npy_path(study_area_id, config_id, chunk_id)
-        logging.info(f"Saving chunk to {path} with shape {prediction.shape}")
-        blob = self.client.get_bucket(self.output_bucket).blob(path)
-        with blob.open("wb") as fd:
-            np.save(fd, prediction)
-
-    def load_prediction_npy(
-        self, study_area_id: str, config_id: str, chunk_id: str
-    ) -> np.ndarray:
-        """Loads a prediction npy file from GCS."""
-        blob = self.client.get_bucket(self.output_bucket).blob(
-            self.get_prediction_npy_path(study_area_id, config_id, chunk_id)
-        )
-        with blob.open("rb") as fd:
-            return np.load(fd)
+    batch_size: int = 2  # Ideally 2 X number of GPUs when using T4.
 
     def predict_and_save_scenario(
         self,
@@ -65,7 +51,18 @@ class BatchPredictor:
         config_id: str,
         scenario_id: str,
     ):
-        """Runs predictions for a rainfall scenario and saves outputs to GCS."""
+        """Runs predictions for a rainfall scenario and saves outputs to GCS.
+
+        This method runs batch prediction over an entire study area with the given
+        rainfall config chunk-by-chunk. Each chunk is stored in an intermediate .npy
+        file. After all chuunks are processed, theya re bundled into a single .jsonl
+        file as expected by the frontend and the .npy files are deleted.
+
+        Args:
+            study_area_id: Study area ID.
+            config_id: Rainfall config ID.
+            scenario_id: Scenario ID.
+        """
         parsed_config_id = urllib.parse.quote_plus(config_id)
         config_dict = (
             self.db.collection("city_cat_rainfall_configs")
@@ -84,6 +81,9 @@ class BatchPredictor:
         for results in self.model.batch_predict_n(dataset, n=rainfall_duration):
             for result in results:
                 chunk_id = result["chunk_id"]
+
+                # When the dataset is distributed, strings are converted to
+                # tensors, so we must decode them back.
                 if isinstance(chunk_id, tf.Tensor):
                     chunk_id = chunk_id.numpy().decode("utf-8")
 
@@ -95,16 +95,10 @@ class BatchPredictor:
                     prediction=result["prediction"].numpy(),
                 )
 
-        chunk_metadata = self.get_chunk_metadata(
-            study_area_id=study_area_id,
-            config_id=config_id,
-            chunk_ids=chunk_ids,
-        )
-
         self.set_model_run_prediction_metadata(
             study_area_id=study_area_id,
             scenario_id=scenario_id,
-            chunk_metadata=chunk_metadata,
+            chunk_ids=chunk_ids,
         )
 
         self.bundle_predictions_to_jsonl(
@@ -114,6 +108,46 @@ class BatchPredictor:
             scenario_id=scenario_id,
         )
 
+    def get_prediction_npy_path(
+        self, study_area_id: str, config_id: str, chunk_id: str
+    ) -> str:
+        """Returns the GCS filepath for a prediction npy file."""
+        return (
+            f"{self.run_id}/flood/{self.model_id}/{study_area_id}"
+            + f"/{config_id}/{chunk_id}"
+        )
+
+    def get_prediction_jsonl_path(self, study_area_id: str, scenario_id: str) -> str:
+        return (
+            f"{self.run_id}/flood/{self.model_id}/{study_area_id}"
+            + f"/{scenario_id}/prediction.results-1-of-1"
+        )
+
+    def save_prediction_npy(
+        self, study_area_id: str, config_id: str, chunk_id: str, prediction: np.ndarray
+    ) -> None:
+        """Saves a prediction npy file to GCS."""
+        path = self.get_prediction_npy_path(study_area_id, config_id, chunk_id)
+        logging.info(f"Saving chunk to {path} with shape {prediction.shape}")
+        blob = self.client.get_bucket(self.output_bucket).blob(path)
+        with blob.open("wb") as fd:
+            np.save(fd, prediction)
+
+    def load_prediction_npy(
+        self, study_area_id: str, config_id: str, chunk_id: str, delete: bool = False
+    ) -> np.ndarray:
+        """Loads a prediction npy file from GCS."""
+        blob = self.client.get_bucket(self.output_bucket).blob(
+            self.get_prediction_npy_path(study_area_id, config_id, chunk_id)
+        )
+        with blob.open("rb") as fd:
+            prediction = np.load(fd)
+
+        if delete:
+            blob.delete()
+
+        return prediction
+
     def set_model_run_metadata(self, scenario_ids: list[str]):
         """Sets model run metadata in firestore."""
         model_ref = self.db.collection("models").document(self.model_id)
@@ -121,40 +155,33 @@ class BatchPredictor:
         run_ref.set({"scenario_ids": scenario_ids})
 
     def set_model_run_prediction_metadata(
-        self,
-        study_area_id: str,
-        scenario_id: str,
-        chunk_metadata: list[dict],
+        self, study_area_id: str, scenario_id: str, chunk_ids: list[str]
     ):
         """Sets the model run prediction metadata in firestore."""
         model_ref = self.db.collection("models").document(self.model_id)
         run_ref = model_ref.collection("runs").document(self.run_id)
         prediction_id = f"Prediction-{study_area_id}-{scenario_id}"
         prediction_ref = run_ref.collection("predictions").document(prediction_id)
+
+        chunks = self.get_chunk_metadata(chunk_ids)
+
         prediction_ref.set(
             {
                 "study_area_id": study_area_id,
                 "scenario_configuration_id": scenario_id,
-                "chunks": chunk_metadata,
+                "path": self.get_prediction_jsonl_path(study_area_id, scenario_id),
+                "chunks": chunks,
             }
         )
 
-    def get_chunk_metadata(
-        self, study_area_id: str, config_id: str, chunk_ids: list[str]
-    ) -> list[dict]:
+    def get_chunk_metadata(self, chunk_ids: list[str]) -> list[dict]:
         """Returns a list of chunk metadata."""
-        chunk_metadata = []
-        for chunk_id in chunk_ids:
-            path = self.get_prediction_npy_path(
-                study_area_id=study_area_id, config_id=config_id, chunk_id=chunk_id
-            )
-            chunk_metadata.append(
-                {
-                    "id": chunk_id,
-                    "path": f"gs://{self.output_bucket}/{path}",
-                }
-            )
-        return chunk_metadata
+        return [
+            {
+                "id": chunk_id,
+            }
+            for chunk_id in chunk_ids
+        ]
 
     @staticmethod
     def prediction_to_json(prediction: np.ndarray, chunk_id: str) -> dict:
@@ -175,16 +202,13 @@ class BatchPredictor:
         scenario_id: str,
     ):
         """Convert npy files to a single jsonl file."""
-        jsonl_path = (
-            f"{self.run_id}/flood/{self.model_id}/{study_area_id}"
-            + f"/{scenario_id}/prediction.results-1-of-1"
-        )
+        jsonl_path = self.get_prediction_jsonl_path(study_area_id, scenario_id)
         with self.client.bucket(self.output_bucket).blob(jsonl_path).open("w") as fd:
             for chunk_id in chunk_ids:
                 path = self.get_prediction_npy_path(study_area_id, config_id, chunk_id)
                 logging.info(f"Bundling {path}...")
                 prediction = self.load_prediction_npy(
-                    study_area_id, config_id, chunk_id
+                    study_area_id, config_id, chunk_id, delete=True
                 )
                 json_data = self.prediction_to_json(prediction, chunk_id)
                 json.dump(json_data, fd)
