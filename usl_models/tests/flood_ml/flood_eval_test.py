d779ff26afff7cc64a4fee86aaf2f66862db1dff
Tatiana Wu <tatianawu@google.com>
2024-07-30 11:10:32 -0700

    Update eval metrics to default to a single example.
    Since model prediction is so expensive, batch computations won't
    be significantly faster, so the functions were updated to assume
    a single prediction/label pair.
    
    If a batch dimension is included, it's aggregated into the metric
    calculation.

--- usl_models/tests/flood_ml/flood_eval_test.py
+++ usl_models/tests/flood_ml/flood_eval_test.py
@@ -8,10 +8,27 @@ from usl_models.flood_ml import eval
 
 def test_spatial_mae():
     """Tests spatial MAE calculation."""
-    label = tf.reshape(tf.range(18, dtype=tf.float64), (2, 3, 3))
+    label = tf.reshape(tf.range(9, dtype=tf.float64), (3, 3))
     pred = label * 1.01
 
     actual = eval.spatial_mae(pred, label)
+    expected = tf.convert_to_tensor(
+        [
+            [0.0, 0.01, 0.02],
+            [0.03, 0.04, 0.05],
+            [0.06, 0.07, 0.08],
+        ],
+        dtype=tf.float64,
+    )
+    np.testing.assert_allclose(actual, expected, rtol=1e-6)
+
+
+def test_batch_spatial_mae():
+    """Tests spatial MAE calculation over a batch."""
+    label = tf.reshape(tf.range(18, dtype=tf.float64), (2, 3, 3))
+    pred = label * 1.01
+
+    actual = eval.spatial_mae(pred, label, batch=True)
     expected = tf.convert_to_tensor(
         [
             [0.045, 0.055, 0.065],
@@ -25,21 +42,43 @@ def test_spatial_mae():
 
 def test_temporal_mae():
     """Tests temporal MAE calculation."""
+    label = tf.reshape(tf.range(36, dtype=tf.float64), (4, 3, 3))
+    error = tf.range(4, dtype=tf.float64) / 10
+    pred = label + tf.tile(error[:, tf.newaxis, tf.newaxis], (1, 3, 3))
+
+    actual = eval.temporal_mae(pred, label)
+    expected = tf.convert_to_tensor([0.0, 0.1, 0.2, 0.3], dtype=tf.float64)
+    np.testing.assert_allclose(actual, expected, rtol=1e-6)
+
+
+def test_batch_temporal_mae():
+    """Tests temporal MAE calculation over a batch."""
     label = tf.reshape(tf.range(72, dtype=tf.float64), (2, 4, 3, 3))
     error = tf.reshape(tf.range(8, dtype=tf.float64), (2, 4)) / 10
     pred = label + tf.tile(error[:, :, tf.newaxis, tf.newaxis], (1, 1, 3, 3))
 
-    actual = eval.temporal_mae(pred, label)
+    actual = eval.temporal_mae(pred, label, batch=True)
     expected = tf.convert_to_tensor([0.2, 0.3, 0.4, 0.5], dtype=tf.float64)
     np.testing.assert_allclose(actual, expected, rtol=1e-6)
 
 
 def test_temporal_rmse():
     """Tests temporal RMSE calculation."""
+    label = tf.reshape(tf.range(36, dtype=tf.float64), (4, 3, 3))
+    error = tf.range(4, dtype=tf.float64) / 10
+    pred = label + tf.tile(error[:, tf.newaxis, tf.newaxis], (1, 3, 3))
+
+    actual = eval.temporal_rmse(pred, label)
+    expected = tf.convert_to_tensor([0.0, 0.1, 0.2, 0.3], dtype=tf.float64)
+    np.testing.assert_allclose(actual, expected, rtol=1e-6)
+
+
+def test_batch_temporal_rmse():
+    """Tests temporal RMSE calculation over a batch."""
     label = tf.reshape(tf.range(72, dtype=tf.float64), (2, 4, 3, 3))
     error = tf.reshape(tf.range(8, dtype=tf.float64), (2, 4)) / 10
     pred = label + tf.tile(error[:, :, tf.newaxis, tf.newaxis], (1, 1, 3, 3))
 
-    actual = eval.temporal_rmse(pred, label)
-    expected = tf.sqrt([0.08, 0.13, 0.2, 0.29])
+    actual = eval.temporal_rmse(pred, label, batch=True)
+    expected = expected = tf.sqrt([0.08, 0.13, 0.2, 0.29])
     np.testing.assert_allclose(actual, expected, rtol=1e-6)
--- usl_models/usl_models/flood_ml/eval.py
+++ usl_models/usl_models/flood_ml/eval.py
@@ -1,58 +1,91 @@
-"""Evaluation metrics for FloodML."""
+"""Evaluation metrics for FloodML.
+
+By default, all the calculations are assumed to be over a single prediction/label
+due to the computational cost of model prediction. If you want to calculate
+metrics *per prediction* over a batch, they should be unstacked into individual
+examples:
+
+```
+for predictions, labels in batch_predict():
+    for prediction, label in zip(tf.unstack(predictions), tf.unstack(labels)):
+        # calculate metrics
+```
+
+Otherwise, specifying `batch = True` in any of the metric functions will average
+over the examples.
+"""
 
 import tensorflow as tf
 
 
-def spatial_mae(predictions: tf.Tensor, labels: tf.Tensor) -> tf.Tensor:
-    """Calculates spatial MAE over a batch of predictions.
+def spatial_mae(
+    predictions: tf.Tensor, labels: tf.Tensor, batch: bool = False
+) -> tf.Tensor:
+    """Calculates spatial MAE.
 
     Args:
-        predictions: [N, H, W] tensor of predictions.
-        labels: [N, H, W] tensor of ground-truth labels.
+        predictions: [H, W] tensor of predictions.
+        labels: [H, W] tensor of ground-truth labels.
+        batch: Whether the inputs include a batch dimension, in which case they
+            have shape [N, H, W].
 
     Returns:
-        [H, W] tensor of the absolute error, averaged over the N examples.
+        [H, W] tensor of the MAE. Note that in the absence of a batch dimension,
+        this is equivalent to the absolute error.
     """
-    return tf.reduce_mean(tf.abs(predictions - labels), axis=0)
+    abs_error = tf.abs(predictions - labels)
+    if batch:
+        return tf.reduce_mean(abs_error, axis=0)
+    return abs_error
 
 
-def temporal_mae(predictions: tf.Tensor, labels: tf.Tensor) -> tf.Tensor:
+def temporal_mae(
+    predictions: tf.Tensor, labels: tf.Tensor, batch: bool = False
+) -> tf.Tensor:
     """Calculates MAE over a time series.
 
     Calculates the MAE separately at each time step. This is useful to evaluate
     whether prediction errors are propagated over the course of the autoregression
     (i.e., if errors are increasing over time).
 
-    We expect T to be constant.
+    If passing in a batch of predictions, we compute the error over the batch
+    dimension too.
 
     Args:
-        predictions: [N, T, H, W] tensor of predictions.
-        labels: [N, T, H, W] tensor of ground-truth labels.
+        predictions: [T, H, W] tensor of predictions.
+        labels: [T, H, W] tensor of ground-truth labels.
+        batch: Whether the inputs include a batch dimension, in which case they
+            have shape [N, T, H, W].
 
     Returns:
         T-length tensor of the MAE.
     """
     # We want to aggregate over all axes except time.
-    axes = (0, 2, 3)
+    axes = (0, 2, 3) if batch else (1, 2)
     return tf.reduce_mean(tf.abs(predictions - labels), axis=axes)
 
 
-def temporal_rmse(predictions: tf.Tensor, labels: tf.Tensor) -> tf.Tensor:
+def temporal_rmse(
+    predictions: tf.Tensor, labels: tf.Tensor, batch: bool = False
+) -> tf.Tensor:
     """Calculates RMSE over a time series.
 
     Calculates the RMSE separately at each time step. This is useful to evaluate
     whether prediction errors are propagated over the course of the autoregression
     (i.e., if errors are increasing over time).
 
-    We expect T to be constant.
+    If passing in a batch of predictions, we compute the error over the batch
+    dimension too.
 
     Args:
-        predictions: [N, T, H, W] tensor of predictions.
-        labels: [N, T, H, W] tensor of ground-truth labels.
+        predictions: [T, H, W] tensor of predictions.
+        labels: [T, H, W] tensor of ground-truth labels.
+        batch: Whether the inputs include a batch dimension, in which case they
+            have shape [N, T, H, W].
 
     Returns:
         T-length tensor of the RMSE.
     """
     # We want to aggregate over all axes except time.
-    axes = (0, 2, 3)
+    axes = (0, 2, 3) if batch else (1, 2)
     return tf.sqrt(tf.reduce_mean((predictions - labels) ** 2, axis=axes))
