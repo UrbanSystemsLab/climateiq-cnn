5039732b4b213ead6fa8834ea3313108bfa2c38a
SallyElHajjar <elhajjas@newschool.edu>
2024-10-06 13:22:39 -0400

    correction to main_type ignore

--- usl_pipeline/cloud_functions/main.py
+++ usl_pipeline/cloud_functions/main.py
@@ -951,7 +951,7 @@ def _build_flood_feature_matrix_from_archive(
 def _build_wps_feature_matrix(fd: IO[bytes]) -> Tuple[NDArray, FeatureMetadata]:
     # Ignore type checker error - BytesIO inherits from expected type BufferedIOBase
     # https://shorturl.at/lk4om
-    with xarray.open_dataset(fd, engine="h5netcdf") as ds:
+    with xarray.open_dataset(fd, engine="h5netcdf") as ds:  # type: ignore
         # Drop the existing 'Time' coordinate if present, to avoid conflict
         if "Time" in ds.coords:
             ds = ds.drop_vars("Time")
