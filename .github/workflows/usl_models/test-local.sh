a0c9db5b00afe1e9e57db4ebf40236486455baf0
Sally <elhajjas@newschool.edu>
2024-12-12 16:40:00 +0000

    uncomment test-local

--- .github/workflows/usl_models/test-local.sh
+++ .github/workflows/usl_models/test-local.sh
@@ -1,6 +1,6 @@
 # .github/workflows/usl_models/test-local.sh
 # if it fails, we should give permission: chmod +x /home/elhajjas/climateiq-cnn/.github/workflows/usl_models/test-local.sh
-# flake8 usl_models --show-source --statistics
-# black usl_models --check
-# pytest usl_models -k "not integration"
-# mypy usl_models
+flake8 usl_models --show-source --statistics
+black usl_models --check
+pytest usl_models -k "not integration"
+mypy usl_models
